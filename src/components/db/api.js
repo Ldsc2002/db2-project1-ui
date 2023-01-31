e050242b7215b12256d85cf747d4baf10ecc5e9b
Ldsc2002 <luissantoscuellar@gmail.com>
2023-01-31 16:58:55 -0600

    Added permanent proxy

--- src/components/db/api.js
+++ src/components/db/api.js
@@ -1,4 +1,5 @@
-const DB_URI = 'https://justcors.com/tl_8f8a771/https://data.mongodb-api.com/app/data-gzxjz/endpoint/data/v1/action/'
+const CORS_PROXY = 'https://justcors.com/l_8rvjduf2bs/'
+const DB_URI = CORS_PROXY + 'https://data.mongodb-api.com/app/data-gzxjz/endpoint/data/v1/action/'
 const API_KEY = 'DycHKf7QxiRTFXE9rLSSDGcEf8xrKaIboqVbWfK8B7xcPbuLptVTTbtWHK3Bp1ng'
 const CLUSTER = 'Cluster0'
 const DB = 'project1'
